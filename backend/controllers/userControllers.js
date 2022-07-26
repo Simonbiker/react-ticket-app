f995ae1f4bf706b32bd9ac275a95464db1619299
Simonbiker <smacr25@gmail.com>
2022-07-26 10:59:16 +0100

    Creating backend methods for register and login for users.

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,12 @@
+const registerUser = (request, response) => {
+    response.send('Register Route')
+}
+
+const loginUser = (request, response) => {
+    response.send('Register Route')
+}
+
+modules.exports = {
+    registerUser,
+    loginUser,
+}
