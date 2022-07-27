03d7812a7c951489781abee804bda7826d9d9a55
Simonbiker <smacr25@gmail.com>
2022-07-27 12:00:51 +0100

    Creating db js file to connect to moongose db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 // entery point to back end
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 8000;
+
+// Connnect to database
+connectDB()
 
 const app = express()
 
