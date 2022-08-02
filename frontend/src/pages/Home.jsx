ea68a9450fea9ce4475e45dced43ebfc99f61911
Simonbiker <smacr25@gmail.com>
2022-08-02 15:47:51 +0100

    Adding links to the home page.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,20 @@
+import {Link} from 'react-router-dom'
+import {FaQuestionCircle, FaTicketAlt} from 'react-icons/fa'
+
 function Home() {
     return (
-        <div>Home Page</div>
+        <>
+            <section className='heading'>
+                <h1>What do you need help with</h1>
+                <p>Please choose from option below</p>
+            </section>
+            <Link to='/new-tickets' className='btn btn-reverse btn-block'>
+                <FaQuestionCircle /> Create a new ticket
+            </Link>
+            <Link to='/tickets' className='btn btn-block'>
+                <FaTicketAlt />View my Tickets
+            </Link>
+        </>
     )
 }
 
