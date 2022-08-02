769ee5ac6f911df4d2f52125e267878f510fc174
Simonbiker <smacr25@gmail.com>
2022-08-02 14:15:34 +0100

    Adding component folder and adding header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// Components
+import Header from './components/Header';
+// Pages
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -11,6 +14,7 @@ function App() {
     <>
       <Router>
         <div className="container">
+          <Header />
           <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path='/login' element={<Login/>}></Route>
